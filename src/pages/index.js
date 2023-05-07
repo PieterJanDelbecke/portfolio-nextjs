834c10104431f73d2eac51c5e289878a23da2ac0
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-05-08 09:25:57 +1000

    styling button

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,21 @@ export default function Home() {
         <meta name="description" content="created by PJD" />
         <link tel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1>develepedbypjd</h1>
-            <ul>
+            <h1 className="text-xl">develepedbypjd</h1>
+            <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill />
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
-                <a href="#">Resume</a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
