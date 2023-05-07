bc4b6b3a7709080d102fa995f2c15d72a4f1a8a4
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-05-08 08:55:52 +1000

    restart

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,26 +9,7 @@ export default function Home() {
         <meta name="description" content="created by PJD" />
         <link tel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white px-10">
-        <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">developedbypjd</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
-              </li>
-              <li>
-                <a
-                  className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </section>
-      </main>
+      <main></main>
     </div>
   );
 }
