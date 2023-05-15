89974c9aa1ce5aa10871823c6d50e1abc0113267
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-05-16 09:41:33 +1000

    styling of cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,8 +73,54 @@ export default function Home() {
             </p>
           </div>
           <div>
-            <div>
-              <Image src={design} alt="design" width={100} height={100} />
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <Image
+                className="mx-auto"
+                src={design}
+                alt="design"
+                width={100}
+                height={100}
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following the
+                core design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <Image
+                className="mx-auto"
+                src={code}
+                alt="design"
+                width={100}
+                height={100}
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following the
+                core design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <Image
+                className="mx-auto"
+                src={consulting}
+                alt="design"
+                width={100}
+                height={100}
+              />
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Beautiful Designs
               </h3>
