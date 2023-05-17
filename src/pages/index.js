80f47ec29675d43a334947fb58aa1c7899dc70a9
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-05-18 08:55:37 +1000

    restyling of section 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,7 +135,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section>
+        {/* <section>
           <div>
             <h3 className="text-3xl py-1">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800">
@@ -151,7 +151,7 @@ export default function Home() {
               programming and teaching
             </p>
           </div>
-        </section>
+        </section> */}
       </main>
     </div>
   );
